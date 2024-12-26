eeac966fb706fe8442ccd471e6783ca2d521232b
AsherHoltham <asher.holtham.github@gmail.com>
2024-12-26 11:34:02 -0500

    Geist Gone

--- pgc/src/app/layout.tsx
+++ pgc/src/app/layout.tsx
@@ -1,17 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Navigation } from "./components/navigation";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -34,7 +23,7 @@ export default function RootLayout({
         <main className="flex-grow">
           {children}
         </main>
-        
+
         <footer className="bg-[#18181b] text-white p-4 text-center">
             PGC
         </footer>
